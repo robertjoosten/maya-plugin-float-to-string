175c085b763be546163f8db2d3b9fcb6103e00ca
Robert Joosten <rwm.joosten@gmail.com>
2017-11-09 20:43:53 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # rjFloatToString
-A Maya node that can convert a float to a string so it can be used in combination with an annotation.
+<img align="right" src="https://github.com/robertjoosten/rjFloatToString/blob/master/icons/floatToString.png">
+This plugin will register a Maya node that can convert a float to a string so it can be used in combination with an annotation. The display precision can be adjusted and so can a prefix and suffix be added.
+
+## Installation
+Copy the **rjFloatToString.py** file in any of the directories that are in your MAYA_PLUG_IN_PATH environment variable:
+> C:\Program Files\Autodesk\<MAYA_VERSION>\plug-ins
+
+Copy all the png files in any of the directories that are in your XBMLANGPATH environment variable:
+> C:\Program Files\Autodesk\<MAYA_VERSION>\icons
+
+## Usage     
+**Node:**
+```python
+from maya import cmds
+
+# create nodes
+floatToString = cmds.createNode("floatToString")
+annotation = cmds.createNode("annotationShape")
+
+# set attributes
+cmds.setAttr("{0}.precision".format(floatToString), 1)
+cmds.setAttr("{0}.prefix".format(floatToString), "height:", type="string")
+cmds.setAttr("{0}.suffix".format(floatToString), "mm", type="string")
+
+# connect to annotation
+cmds.connectAttr(
+    "{0}.output".format(floatToString),
+    "{0}.text".format(annotation)
+)
+```
+        
+        
---
+++ plug-ins/rjFloatToString.py
@@ -0,0 +1,206 @@
+"""			
+This plugin will register a Maya node that can convert a float to a string so 
+it can be used in combination with an annotation. The display precision can 
+be adjusted and so can a prefix and suffix be added.
+
+
+.. figure:: https://github.com/robertjoosten/rjFloatToString/blob/master/icons/floatToString.png
+   :align: center
+
+Installation
+============
+Copy the "rjFloatToString.py" file in any of the directories that are in your 
+MAYA_PLUG_IN_PATH environment variable   
+::
+    C://Program Files//Autodesk//<MAYA_VERSION>//plug-ins
+    
+Copy all the png files in any of the directories that are in your 
+XBMLANGPATH environment variable
+::
+    C://Program Files//Autodesk//<MAYA_VERSION>//icons
+		
+Usage
+=====
+Node
+::
+    from maya import cmds
+
+    # create nodes
+    floatToString = cmds.createNode("floatToString")
+    annotation = cmds.createNode("annotationShape")
+
+    # set attributes
+    cmds.setAttr("{0}.precision".format(floatToString), 1)
+    cmds.setAttr("{0}.prefix".format(floatToString), "height:", type="string")
+    cmds.setAttr("{0}.suffix".format(floatToString), "mm", type="string")
+
+    # connect to annotation
+    cmds.connectAttr(
+        "{0}.output".format(floatToString),
+        "{0}.text".format(annotation)
+    )
+"""
+
+__author__   = "Robert Joosten"
+__version__  = "0.1.0"
+__email__    = "rwm.joosten@gmail.com"
+
+from maya import OpenMaya, OpenMayaMPx
+
+class FloatToStringNode(OpenMayaMPx.MPxNode):
+    kPluginNodeName = "floatToString"
+    kPluginNodeId = OpenMaya.MTypeId(0x31550)
+    kPluginNodeClassify = "utility/general"
+    
+    def __init__(self):
+        OpenMayaMPx.MPxNode.__init__(self)
+
+    def compute(self, plug, data):
+        # get input
+        floatData = data.inputValue(self.float)
+        floatValue = floatData.asFloat()
+        
+        precisionData = data.inputValue(self.precision)
+        precisionValue = precisionData.asInt()
+        
+        prefixData = data.inputValue(self.prefix)
+        prefixValue = prefixData.asString()
+      
+        suffixData = data.inputValue(self.suffix)
+        suffixValue = suffixData.asString()
+        
+        # set output
+        outputVolume = data.outputValue(self.output)
+        outputVolume.setString(
+            "{p} {n:.{d}f} {s}".format(
+                n=floatValue, 
+                d=precisionValue,
+                p=prefixValue,
+                s=suffixValue
+            ).strip()
+        )
+        
+        data.setClean(plug)
+
+    @classmethod
+    def nodeCreator(cls):
+        return OpenMayaMPx.asMPxPtr(cls())
+
+    @classmethod
+    def nodeInitializer(cls):  
+        # input
+        floatAttr = OpenMaya.MFnNumericAttribute()
+        cls.float = floatAttr.create(
+            "float", 
+            "f", 
+            OpenMaya.MFnNumericData.kFloat, 
+            0.0
+        )
+        floatAttr.setKeyable(True)
+        
+        precisionAttr = OpenMaya.MFnNumericAttribute()
+        cls.precision = precisionAttr.create(
+            "precision", 
+            "prec", 
+            OpenMaya.MFnNumericData.kInt, 
+            3
+        )
+        precisionAttr.setKeyable(True)
+        
+        stringData = OpenMaya.MFnStringData().create("")
+        prefixAttr = OpenMaya.MFnTypedAttribute()
+        cls.prefix = prefixAttr.create(
+            "prefix", 
+            "pref", 
+            OpenMaya.MFnData.kString, 
+            stringData
+        )
+        
+        stringData = OpenMaya.MFnStringData().create("")
+        suffixAttr = OpenMaya.MFnTypedAttribute()
+        cls.suffix = prefixAttr.create(
+            "suffix", 
+            "suf", 
+            OpenMaya.MFnData.kString, 
+            stringData
+        )
+        
+        # output
+        stringData = OpenMaya.MFnStringData().create("")
+        outputAttr = OpenMaya.MFnTypedAttribute()
+        cls.output = outputAttr.create(
+            "output", 
+            "o", 
+            OpenMaya.MFnData.kString, 
+            stringData
+        )
+        outputAttr.setWritable(False)
+
+        # add attributes
+        cls.addAttribute(cls.float)  
+        cls.addAttribute(cls.precision)  
+        cls.addAttribute(cls.prefix)  
+        cls.addAttribute(cls.suffix) 
+        cls.addAttribute(cls.output) 
+
+        # add dependencies
+        cls.attributeAffects(cls.float, cls.output)
+        cls.attributeAffects(cls.precision, cls.output)
+        cls.attributeAffects(cls.prefix, cls.output)
+        cls.attributeAffects(cls.suffix, cls.output)
+
+# ---------------------------------------------------------------------------------------
+
+def initializePlugin(mObject):
+    mPlugin = OpenMayaMPx.MFnPlugin(mObject)
+    try:        
+        mPlugin.registerNode(
+            FloatToStringNode.kPluginNodeName, 
+            FloatToStringNode.kPluginNodeId, 
+            FloatToStringNode.nodeCreator, 
+            FloatToStringNode.nodeInitializer
+        )
+    except:     
+        raise RuntimeError(
+            "Failed to register : {0}".format(
+                FloatToStringNode.kPluginNodeName
+            )
+        )
+
+def uninitializePlugin(mObject):
+    mPlugin = OpenMayaMPx.MFnPlugin(mObject)
+    try:        
+        mPlugin.deregisterNode(
+            FloatToStringNode.kPluginNodeId
+        )
+    except:     
+        raise RuntimeError(
+            "Failed to deregister : {0}".format(
+                FloatToStringNode.kPluginNodeName
+            )
+        )
+    
+# ---------------------------------------------------------------------------------------
+
+AETemplateCommand = """
+global proc AEfloatToStringTemplate( string $nodeName )
+{
+    editorTemplate -beginScrollLayout;
+ 
+    editorTemplate -beginLayout "Input" -collapse 0;
+        editorTemplate -addControl "float";
+        editorTemplate -addControl "precision";
+        editorTemplate -addControl "prefix";
+        editorTemplate -addControl "suffix";
+    editorTemplate -beginLayout "Output" -collapse 0;
+        editorTemplate -addControl "output";
+  
+    editorTemplate -endLayout;
+ 
+    AEdependNodeTemplate $nodeName;
+ 
+    editorTemplate -addExtraControls;
+    editorTemplate -endScrollLayout;
+}
+"""
+OpenMaya.MGlobal.executeCommand(AETemplateCommand, False, False)
